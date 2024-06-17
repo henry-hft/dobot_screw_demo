1d7239e660b316daa58251ee81ae4247638e718d
henry-hft <333333henry@gmail.com>
2024-06-17 15:55:04 +0200

    feat: flask enable debug

--- dobot_server.py
+++ dobot_server.py
@@ -259,4 +259,4 @@ if __name__ == "__main__":
     #app.run(host="192.168.11.151", port=5000)
     setup_dobot()
     setup_calibration()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
