f5d437e21deb41e4ecd936cb1b44ea9e2d04c6a7
henry-hft <333333henry@gmail.com>
2024-11-18 12:33:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,28 +5,6 @@ ETH_INTERFACE="eth0"
 DOBOT_IP="192.168.1.6"
 PI_IP="192.168.1.5"
 
-# Function to check if conda is installed
-function check_conda_installed() {
-    if ! command -v conda &> /dev/null; then
-        echo "Miniconda is not installed. Installing Miniconda..."
-        install_miniconda
-    else
-        echo "Miniconda is already installed."
-        source "$(conda info --base)/etc/profile.d/conda.sh"
-    fi
-}
-
-# Function to install Miniconda
-function install_miniconda() {
-    MINICONDA_SCRIPT="Miniconda3-latest-Linux-x86_64.sh"
-    wget https://repo.anaconda.com/miniconda/$MINICONDA_SCRIPT -O /tmp/$MINICONDA_SCRIPT
-    bash /tmp/$MINICONDA_SCRIPT -b -p $HOME/miniconda3
-    rm /tmp/$MINICONDA_SCRIPT
-    source "$HOME/miniconda3/etc/profile.d/conda.sh"
-    conda init
-    echo "Miniconda installation complete. Please restart your shell or run 'source ~/.bashrc'."
-}
-
 # Start of the main script
 echo "Configuring Ethernet for the Dobot connection..."
 sudo ip link set $ETH_INTERFACE up
@@ -37,8 +15,15 @@ sudo ip route add $DOBOT_IP dev $ETH_INTERFACE
 sudo apt-get update
 sudo apt-get install -y libblas-dev liblapack-dev libatlas-base-dev gfortran python3-dev python3-setuptools python3-opencv python3-picamera2 python3-matplotlib
 
-# Check and install Miniconda if necessary
-check_conda_installed
+# Install Miniconda
+echo "Installing Miniconda..."
+sudo mkdir -p ~/miniconda3
+sudo wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+sudo bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+sudo rm ~/miniconda3/miniconda.sh
+sudo source ~/miniconda3/bin/activate
+conda init --all
+echo "Miniconda installation complete. Please restart your shell or run 'source ~/.bashrc'."
 
 # Create a new conda environment
 ENV_NAME="dobot_env"
